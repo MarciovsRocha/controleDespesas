15e53cb4b3822d707e0cd9217cca2477b3c3ef4b
Marcio Vinicius de Souza da Rocha <dev.marcio.rocha@gmail.com>
2023-01-17 20:53:55 -0300

    Criado projeto C# via RIDER IDEA

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControleDespesas
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
